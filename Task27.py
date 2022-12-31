6733186aee02a3aab0fd93a60e3e3f57e038c993
NovartGITHUB <119023979+NovartGITHUB@users.noreply.github.com>
2022-12-31 02:21:28 +0100

    Update Task27.py

--- Task27.py
+++ Task27.py
@@ -1,4 +1,4 @@
-#  Write the program that checks if any data put by the user is alphanumeric string.
+#  Below is a program that checks if any data put by the user is alphanumeric string.
 
 Data = input("Put the text: ")
 print(Data, Data.isalnum())

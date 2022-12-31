2e4d49299b037e30ed6516f300bb32b67e7e5083
NovartGITHUB <119023979+NovartGITHUB@users.noreply.github.com>
2022-12-31 02:11:57 +0100

    Update Task31.py

--- Task31.py
+++ Task31.py
@@ -1,3 +1,5 @@
+# Below is a command that randomly chooses day of theweek from a list
+
 import random as rn
 Days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 print('This week, my favorite day is: ' + Days[rn.randrange(0,7)])

80975dbe4620d578a2b7074f090fe9b56c8e2be1
NovartGITHUB <119023979+NovartGITHUB@users.noreply.github.com>
2022-12-31 02:20:44 +0100

    Update Task28.py

--- Task28.py
+++ Task28.py
@@ -1,5 +1,4 @@
-# Prepare the program which creates the initials from the Name and Surname entered
-# by the user (eg. name: John, surname: Doe, initials: JD).
+# Below is a program which creates the initials from the Name and Surname entered by the user (eg. name: John, surname: Doe, initials: JD).
 
 Name = input("Put your name: ")
 Surname = input("Put your surname: ")
